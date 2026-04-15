e2d050b7463c4918874649c005af16c3df602e1f
1nc24is041-dotcom <1nc24is041@ncetmail.com>
2026-04-15 10:43:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,4 +3,4 @@ from groq import Groq
 
 st.set_page_config("PragyanAI Content Generator",layout="wide")
 st.title("PragyanAI-Content Generation")
-st.img(" ")
+st.image("WhatsApp Image 2025-12-27 at 4.03.22 PM.jpeg")
