8ec2857a39ba4f92e8df8cd972322a95ad0381dd
1nc24is041-dotcom <1nc24is041@ncetmail.com>
2026-04-15 11:03:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,3 +4,24 @@ from groq import Groq
 st.set_page_config("PragyanAI Content Generator",layout="wide")
 st.title("PragyanAI-Content Generation")
 st.image("WhatsApp Image 2025-12-27 at 4.03.22 PM.jpeg")
+client=Groq(api_key=st.secrets[GROQ_API_KEY"])
+product=st.text_input("product")
+audience=st.text_input("Audience")
+if st.button("Generate Content for {product} targeting {audience}."
+response=client.chat.completions.create(
+    model="llama-3.3-70b-versatile",
+    messages=[{"role":"user","content":prompt}]
+    )
+    st.session_state.text = response.choices[0].message.content
+    text =response.choices[0].message.content
+    st.write(text)
+if "text" in st.session_state:
+    content = st.text_area("Generated Content", st.session_state.text, height=300)
+    st.download_button(
+            label="⬇️ Download as TXT",
+            data=content,
+            file_name="marketing_copy.txt",
+            mime="text/plain"
+        )
+    else:
+        st.info("Generate content first")
